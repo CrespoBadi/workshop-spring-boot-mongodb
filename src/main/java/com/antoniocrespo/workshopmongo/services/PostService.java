c34f5a4161b60818aef421aee4556d9aec8053c4
Antonio Crespo <antonio.crespo@hexa-consulting.pt>
2024-09-16 13:08:06 +0100

    Obtendo um post por id

---
+++ src/main/java/com/antoniocrespo/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.antoniocrespo.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.antoniocrespo.workshopmongo.domain.Post;
+import com.antoniocrespo.workshopmongo.repository.PostRepository;
+import com.antoniocrespo.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id); 
+		 return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 	
+	}
+	
+}
