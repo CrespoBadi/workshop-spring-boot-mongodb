5ff764de72c96e36929ec9e341791a29d7a78b36
Antonio Crespo <antonio.crespo@hexa-consulting.pt>
2024-09-16 16:01:33 +0100

    Consulta simples com query methods

--- src/main/java/com/antoniocrespo/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/antoniocrespo/workshopmongo/repository/PostRepository.java
@@ -1,12 +1,16 @@
 package com.antoniocrespo.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.antoniocrespo.workshopmongo.domain.Post;
-import com.antoniocrespo.workshopmongo.domain.User;
+
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
